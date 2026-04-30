00747aefbedf19d4da0a2184a8c26f3fc798807e
Nikhil <nikhil.belide@graphy.com>
2026-04-30 14:24:11 +0530

    update cookie config to match cors

--- src/config/cookie.config.js
+++ src/config/cookie.config.js
@@ -1,6 +1,8 @@
 export const cookieConfig = {
   httpOnly: true, // Prevents JS access (XSS protection)
-  sameSite: "strict", // Only sends over HTTPS
+  sameSite: "none", // Only sends over HTTPS
   secure: process.env.NODE_ENV === "production", // Prevents CSRF (Cross-Site Request Forgery)
   path: "/", // Always explicitly set path to avoid route-specific cookies (default path is "/")
+  // Add this to persist the cookie (e.g., for 7 days)
+  maxAge: 7 * 24 * 60 * 60 * 1000,
 };
